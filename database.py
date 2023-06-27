6f4503144f2488958823e07061f50a3f9e2e5271
Javid <53756609+Javiques@users.noreply.github.com>
2023-06-27 10:30:11 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,27 +1,27 @@
-import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+from core import Viewed
 
-metadata = MetaData()
-Base = declarative_base()
+class Database:
+    def __init__(self, db_url):
+        self.engine = create_engine(db_url)
+        self.Session = sessionmaker(bind=self.engine)
 
+    def add_viewed_profile(self, profile_id, worksheet_id):
+        session = self.Session()
+        viewed = Viewed(profile_id=profile_id, worksheet_id=worksheet_id)
+        session.add(viewed)
+        try:
+            session.commit()
+        except SQLAlchemyError as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
 
-class Viewed(Base):
-    __tablename__ = 'viewed'
-    profile_id = sq.Column(sq.Integer, primary_key=True)
-    worksheet_id = sq.Column(sq.Integer, primary_key=True)
-
-
-engine = create_engine(db_url_object)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-to_db = Viewed(profile_id=1, worksheet_id=1)
-session.add(to_db)
-session.commit()
-
-from_db = session.query(Viewed).filter(Viewed.profile_id == 1).all()
-for item in from_db:
-    print(item.worksheet_id)
+    def check_profile_viewed(self, profile_id):
+        session = self.Session()
+        viewed = session.query(Viewed).filter(Viewed.profile_id == profile_id).first()
+        session.close()
+        return viewed is not None
