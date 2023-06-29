d7f2dbc9d6aca00a3a855e7a555137f41b7abee3
Javid <53756609+Javiques@users.noreply.github.com>
2023-06-29 23:24:05 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -24,6 +24,24 @@ class BotInterface():
                                'random_id': get_random_id()}
                               )
 
+    def handle_parameters(self, user_id, command):
+        if command == 'параметры':
+            self.message_send(user_id, 'Введите параметры в формате: возраст, пол, город, семейное положение')
+        else:
+            parameters = command.split(',')
+            if len(parameters) != 4:
+                self.message_send(user_id, 'Некорректный формат параметров. Введите параметры в формате: возраст, пол, город, семейное положение')
+                return
+            
+            age, sex, city, relation = [param.strip() for param in parameters]
+
+            self.params['bdate'] = datetime.now().strftime('%d.%m.%Y')
+            self.params['sex'] = 1 if sex.lower() == 'женский' else 2
+            self.params['city'] = city.strip()
+            self.params['relation'] = relation.strip()
+
+            self.message_send(user_id, 'Параметры успешно обновлены. Вы можете использовать команду "поиск" для поиска пары.')
+
     def event_handler(self):
         longpoll = VkLongPoll(self.interface)
 
@@ -33,27 +51,19 @@ class BotInterface():
 
                 if command == 'привет':
                     self.params = self.api.get_profile_info(event.user_id)
-                    self.message_send(event.user_id, f'здравствуй {self.params["name"]}')
+                    self.message_send(event.user_id, f'Здравствуй, {self.params["name"]}!')
+                elif command == 'параметры' or command.startswith('параметры:'):
+                    self.handle_parameters(event.user_id, command)
                 elif command == 'поиск':
+                    if any(param is None for param in self.params.values()):
+                        self.message_send(event.user_id, 'Для поиска пары необходимо указать параметры. Введите "параметры" для задания параметров.')
+                        continue
+
                     users = self.api.serch_users(self.params)
                     user = users.pop()
 
                     if self.db.check_profile_viewed(user['id']):
                         continue
-
-                    missing_info = []
-                    if not self.params['bdate']:
-                        missing_info.append('дата рождения')
-                    if not self.params['city']:
-                        missing_info.append('город')
-                    if not self.params['sex']:
-                        missing_info.append('пол')
-                    if not self.params['relation']:
-                        missing_info.append('семейное положение')
-
-                    if missing_info:
-                        self.message_send(event.user_id, f'Для поиска пары необходимо указать следующую информацию: {", ".join(missing_info)}')
-                        return
 
                     photos_user = self.api.get_photos(user['id'])
 
@@ -62,13 +72,13 @@ class BotInterface():
                         attachment += f'photo{photo["owner_id"]}_{photo["id"]}'
                         if num == 2:
                             break
-                    self.message_send(event.user_id, f'Встречайте {user["name"]}', attachment=attachment)
+                    self.message_send(event.user_id, f'Встречайте {user["name"]}!', attachment=attachment)
 
                     self.db.add_viewed_profile(user['id'], 1)
                 elif command == 'пока':
-                    self.message_send(event.user_id, 'пока')
+                    self.message_send(event.user_id, 'Пока!')
                 else:
-                    self.message_send(event.user_id, 'команда не опознана')
+                    self.message_send(event.user_id, 'Команда не опознана.')
 
 if __name__ == '__main__':
     community_token = "vk1.a.gDG2onsHHq-pvzLjMTXq4n140XK5PJuDFcV7N7kTBD1ULEf73sNuV9ARBmD23Sf5JXD36UEbw4s484C9_ShMqNJNhGwJ3QGn9UHafypxCTRu2ZLaTuDsQJg8CWfxy5wuA09SU4Xlzcc5tztxLpmJWYfAZ9XUyYqTNDttxrB9UDQNcdGove-vSAZ6zsKU7rI-99JOgIEeTi_yUWb8osFmDA"
