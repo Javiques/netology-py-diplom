52b41228a44d45dfffbe7d21428d77a338857c71
Javid <53756609+Javiques@users.noreply.github.com>
2023-07-07 09:32:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 access_token = 'YOUR_ACCESS_TOKEN'
 comunity_token = 'YOUR_COMMUNITY_TOKEN'
-db_url_object = "postgresql+psycopg2://student:student@localhost/student_diplom"
+db_url_object = 'postgresql+psycopg2://student:student@localhost/student_diplom'
